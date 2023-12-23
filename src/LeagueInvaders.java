aac5d2911d3965d57d3d02d9f0cab4cf126546c2
league <gmjangwoo0827@gmail.com>
2023-12-22 16:52:24 -0800

    bangbangbangabangboom

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,21 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
 
+	JFrame frame = new  JFrame();
+	public final static int WIDTH = 500;
+	public final static int HEIGHT = 800;
+	
+	
+	public static void main(String[] args) {
+		LeagueInvaders vadors = new LeagueInvaders();
+		vadors.setup();
+	}
+	
+	public void setup() {
+		frame.setVisible(true);
+		frame.setSize(WIDTH,HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
 }
